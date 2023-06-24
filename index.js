297b71b7cf1485e1a2411ee031056617a494e2da
v4rgas <66626747+v4rgas@users.noreply.github.com>
2023-06-23 21:59:27 -0400

    writing

--- index.js
+++ index.js
@@ -6,14 +6,25 @@ const letters = "EAOSRNIDLCTUMPBGVYQHFZJÑXKW";
 const letterElement = document.getElementById("letter");
 const left = document.getElementById("tapL");
 const right = document.getElementById("tapR");
-// const delete = document.getElementById("delete");
-// const add = document.getElementById("add");
+const deleteBtn = document.getElementById("delete");
+const addBtn = document.getElementById("add");
+const footer = document.getElementById("footer");
 let currentIndex = 0;
 
 const changeLetter = (num) => {
   currentIndex = (currentIndex + num + letters.length) % letters.length;
-  console.log(currentIndex)
+  console.log(currentIndex);
   letterElement.textContent = letters[currentIndex];
+};
+
+const writeLetter = () => {
+  footer.textContent += letterElement.textContent;
+  console.log("test");
+};
+
+const deleteLetter = () => {
+  footer.textContent = footer.textContent.slice(0, -1);
+  console.log("test");
 };
 
 window.addEventListener("click", () => {
@@ -31,9 +42,15 @@ right.addEventListener("click", () => {
   console.log("test");
 });
 
+addBtn.addEventListener("click", writeLetter);
+
+deleteBtn.addEventListener("click", deleteLetter);
+
 navigator.mediaSession.setActionHandler("play", () => changeLetter(1));
 navigator.mediaSession.setActionHandler("pause", () => changeLetter(1));
-navigator.mediaSession.setActionHandler("nexttrack", () => changeLetter(1));
+navigator.mediaSession.setActionHandler("nexttrack", () => writeLetter);
 navigator.mediaSession.setActionHandler("previoustrack", () =>
-  changeLetter(-1)
+  deleteLetter()
 );
+
+console.log(add);
